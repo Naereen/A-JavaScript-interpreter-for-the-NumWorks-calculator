aefb8072f9dd03b436e3700d6b65b7142d2a3d96
Lilian Besson <lilian.besson@crans.org>
2025-05-26 20:24:07 +0200

    Starts to write the documentation for this tiny 'Eadk' module that I expose to JavaScript.

--- src/test2.js
+++ src/test2.js
@@ -4,27 +4,28 @@
 // Highly experimental: just a for loop, to sleep
 function msleep(s) {
     for (let j = 1; j <= s; j++) {
-      // Just a comment here
+        // Just a comment here
     }
-  }
-  
-  console.log("Hello world from JavaScript!");
-  console.log("Testing Eadk functions:");
-  msleep(5000);
-  
-  const brightness = Eadk.backlight_brightness();
-  console.log("Eadk.backlight_brightness() =", Eadk.backlight_brightness());
-  msleep(2000);
-  
-  for (let b = brightness; b >= 0; b=b-1) {
-    Eadk.set_backlight_brightness(b);
-    console.log("Eadk.backlight_brightness() =", Eadk.backlight_brightness());
-    msleep(100);
-  }
-  
-  for (let b = 0; b <= brightness; b=b+1) {
-    Eadk.set_backlight_brightness(b);
-    console.log("Eadk.backlight_brightness() =", Eadk.backlight_brightness());
-    msleep(100);
-  }
-  
+}
+
+console.log("Hello world from JavaScript!");
+console.log("Testing Eadk functions:");
+msleep(5000);
+
+const brightness = Eadk.backlight_brightness();
+console.log("Eadk.backlight_brightness() =", Eadk.backlight_brightness());
+msleep(2000);
+
+for (let dwarf = 1; dwarf <= 13; dwarf++) {
+    for (let b = brightness; b >= 0; b=b-16) {
+        Eadk.set_backlight_brightness(b);
+        console.log("Eadk.backlight_brightness() =", Eadk.backlight_brightness());
+        msleep(50);
+    }
+
+    for (let b = 0; b <= brightness; b=b+16) {
+        Eadk.set_backlight_brightness(b);
+        console.log("Eadk.backlight_brightness() =", Eadk.backlight_brightness());
+        msleep(50);
+    }
+}
